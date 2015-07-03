58b049a0e3cb3f184682d38f506d95ae609f911f
Amitai Burstein <amitai@gizra.com>
2015-07-03 23:26:12 +0300

    add events

--- src/login.js
+++ src/login.js
@@ -2,8 +2,9 @@ import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {Config} from '../config/config';
 import {HttpClient} from 'aurelia-http-client';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(Router, HttpClient, Config)
+@inject(Config, EventAggregator, HttpClient, Router)
 export class Login {
 
   // Default credentials.
@@ -12,10 +13,11 @@ export class Login {
     pass: '1234'
   }
 
-  constructor(router, http, config) {
-    this.router = router;
-    this.http = http;
+  constructor(config, eventAggregator, http, router) {
     this.config = config;
+    this.eventAggregator = eventAggregator;
+    this.http = http;
+    this.router = router;
   }
 
   get canLogin() {
@@ -26,6 +28,11 @@ export class Login {
     var credentials = this.credentials;
     // Convert to base64.
     var base64 = window.btoa(credentials.username + ':' + credentials.pass);
+
+    this.eventAggregator.subscribe('user_login', payload => {
+      console.log('user logged');
+      console.log(payload);
+    });
 
     return this.http
       .configure(x => {
@@ -38,8 +45,11 @@ export class Login {
         var accessToken = JSON.parse(response.response).access_token;
         localStorage.setItem('access_token', accessToken);
 
-        this.router.navigate('dashboard');
+        console.log('ok');
+
+        // Notify user has logged in.
+        this.eventAggregator.publish('user_login', JSON.parse(response.response));
+
       });
   }
-
 }
