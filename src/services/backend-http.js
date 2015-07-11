6bd9a0601a822d084d1c83f402c5558136e4e3ab
Amitai Burstein <amitai@gizra.com>
2015-07-11 23:31:28 +0300

    Simplify WebAPI

--- src/services/backend-http.js
+++ src/services/backend-http.js
@@ -1,41 +1,22 @@
 import {inject, transient} from 'aurelia-framework';
 import {Auth} from './auth';
 import {Config} from '../config/config';
-import {EventAggregator} from 'aurelia-event-aggregator';
-
 import {HttpClient} from 'aurelia-http-client';
 
 
 @transient()
-@inject(Auth, Config, EventAggregator)
+@inject(Auth, Config)
 export class WebAPI {
 
   constructor(auth, config, eventAggregator) {
     this.auth = auth;
     this.config = config;
-    this.eventAggregator = eventAggregator;
 
     this.http = new HttpClient();
-    this.initHttp();
-
-    this.subscribeEvents();
-  }
-
-  initHttp() {
     this.http
       .configure(x => {
         x.withBaseUrl(this.config.backendUrl);
         x.withHeader('access-token', this.auth.getAccessToken());
-    });
-  }
-
-  subscribeEvents() {
-    this.eventAggregator.subscribe('user_login', payload => {
-      this.initHttp();
-    });
-
-    this.eventAggregator.subscribe('user_logout', payload => {
-      this.initHttp();
     });
   }
 }
