ffd4da99800b851148ee4454b3fe025b92888c6a
Amitai Burstein <amitai@gizra.com>
2015-07-11 23:33:28 +0300

    Revert "Simplify WebAPI"
    This reverts commit 6bd9a0601a822d084d1c83f402c5558136e4e3ab.

--- src/services/backend-http.js
+++ src/services/backend-http.js
@@ -1,24 +1,43 @@
 import {inject, transient} from 'aurelia-framework';
 import {Auth} from './auth';
 import {Config} from '../config/config';
+import {EventAggregator} from 'aurelia-event-aggregator';
+
 import {HttpClient} from 'aurelia-http-client';
 
 
 // Make sure the service is not singleton, as each time we may change the
 // withParams value, an we don't want it to persist.
 @transient()
-@inject(Auth, Config)
+@inject(Auth, Config, EventAggregator)
 export class WebAPI {
 
   constructor(auth, config, eventAggregator) {
     this.auth = auth;
     this.config = config;
+    this.eventAggregator = eventAggregator;
 
     this.http = new HttpClient();
+    this.initHttp();
+
+    this.subscribeEvents();
+  }
+
+  initHttp() {
     this.http
       .configure(x => {
         x.withBaseUrl(this.config.backendUrl);
         x.withHeader('access-token', this.auth.getAccessToken());
+    });
+  }
+
+  subscribeEvents() {
+    this.eventAggregator.subscribe('user_login', payload => {
+      this.initHttp();
+    });
+
+    this.eventAggregator.subscribe('user_logout', payload => {
+      this.initHttp();
     });
   }
 }
